3988d0ce22308594e9fbe2c60edbdfa963c6ee68
Averin Jaroslav <65073167+AverinYaroslav@users.noreply.github.com>
2021-04-26 21:52:20 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+token = '1769700682:AAH_I5nyQI560hFrG29ueDb2imYuozbohe4'
+admin_token = '1789683731:AAEkIe2XIyushf3yFWqKh5VjoKLj5ul3j0Y'
+admin_link = '@Admin'
+admin_chat_id = 0
+db_name = 'users.db'
+information = 'something'
+admin_id = 'U28051194'
+admin_password = 'hJv-svi-q6A-DEf'
+admin_account = 4053971
+start_month, start_day, start_year = 4, 25, 2021
+end_month, end_day, end_year = 5, 23, 2021
